ef0d559b4b8d418c741bf68a8ec70e864a075b1e
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2025-01-14 16:14:25 +0100

    fix cors bug

--- app/main.py
+++ app/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 from .scraper import scrape_events
+from fastapi.middleware.cors import CORSMiddleware
+import asyncio
+import sys
+
 
 app = FastAPI()
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins (replace with specific origins in production)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Define the request body schema
 class ScrapeRequest(BaseModel):
@@ -24,7 +39,7 @@ class ScrapedEvents(BaseModel):
     events: List[Event]
 
 @app.post("/scrape/", response_model=ScrapedEvents)
-async def scrape(request: ScrapeRequest):
+def scrape(request: ScrapeRequest):
     try:
         # Call the scrape_events function with user-provided inputs
         result = scrape_events(
